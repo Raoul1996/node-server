94e8d8c576d0283e255335113b9543e1bc1e1f92
Raoul1996 <baobaozhou199606@gmail.com>
2017-07-06 21:57:13 +0000

    edit node-server

--- step0/index.js
+++ step0/index.js
@@ -1,17 +1,18 @@
 var http = require('http')
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   //setTimeout(function(){
-    
-    //response.setHeader("Content-Type","text/plain; charset=utf-8")
-    response.setHeader('Content-Type','text/html')
-    //response.writeHead(202, 'haha')
-    response.write('<html><head><meta charset="utf-8" /></head>')
-    response.write('<body>')
-    response.write('<h1>你好</h1>')
-    response.write('</body>')
-    
-    response.end()
+
+  //response.setHeader("Content-Type","text/plain; charset=utf-8")
+  response.setHeader('Content-Type', 'text/html')
+  response.writeHead(202, 'haha')
+  //res设置的 charset 优先级最高
+  response.write('<html><head><meta charset="utf-8" /></head>')
+  response.write('<body>')
+  response.write('<h1>你好</h1>')
+  response.write('</body>')
+
+  response.end()
   //},2000);
 })
 

c45a9acd4628d6f567c157ba05ed9737c385c67c
raoul <raoul@localhost.localdomain>
2017-07-06 21:55:17 +0000

    edit node-server

--- step1/server.js
+++ step1/server.js
@@ -4,44 +4,44 @@ var fs = require('fs')
 var url = require('url')
 
 
-function staticRoot(staticPath, req, res){
-  
+function staticRoot(staticPath, req, res) {
+
   var pathObj = url.parse(req.url, true)
-  
-  /*
-  if(pathObj.pathname === '/'){
-    pathObj.pathname += 'index.html'
-  }
-  */
+
+   if(pathObj.pathname === '/'){
+   pathObj.pathname += 'index.html'
+   }
+
 
   var filePath = path.join(staticPath, pathObj.pathname)
-  
-  var fileContent = fs.readFileSync(filePath,'binary')
+
+  var fileContent = fs.readFileSync(filePath, 'binary')
   res.write(fileContent, 'binary')
   res.end()
-  
-  /*
-  fs.readFile(filePath, 'binary', function(err, fileContent){
-    if(err){
+
+
+  fs.readFile(filePath, 'binary', function (err, fileContent) {
+    if (err) {
       console.log('404')
       res.writeHead(404, 'not found')
       res.end('<h1>404 Not Found</h1>')
-    }else{
+    } else {
       console.log('ok')
+      res.writeHead(200, 'OK')
       res.write(fileContent, 'binary')
-      res.end()      
+      res.end()
     }
   })
-  */
+
 
 }
 
-var server = http.createServer(function(req, res){
+var server = http.createServer(function (req, res) {
   staticRoot(path.resolve(__dirname, 'static'), req, res)
 })
 
 server.listen(8080)
-console.log('visit http://localhost:8080' )
+console.log('visit http://localhost:8080')
 
 
 
